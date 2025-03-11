e6b88dfb949fe1eed2e22b1cf7443144c3bdab4c
ChrissDisigale <chrispro.genius@gmail.com>
2025-03-11 17:31:55 +0300

    closes #1, closes #2, closes #3; Bootstrap Application

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
 
   production: false,
-  // apiUrl: 'http://127.0.0.1:8000/', // API endpoint for development
-  apiUrl: 'http://45.79.10.106:8000/npmis/api/v1/', // API endpoint for production
+  apiUrl: 'http://127.0.0.1:8000/', // API endpoint for development
+  // apiUrl: 'http://45.79.10.106:8000/npmis/api/v1/', // API endpoint for production
 };
